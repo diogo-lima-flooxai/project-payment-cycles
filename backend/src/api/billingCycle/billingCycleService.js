87ef25266b2ee63a5420141597b9b3e2b6b88a0d
diogo <diogo.lima.flooxai@gmail.com>
2025-04-29 08:18:49 -0300

    feat: add querying routes with postman and getting the amount of records

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -1,6 +1,26 @@
-const BillingCycle = require('./billingCycle')
+const BillingCycle = require("./billingCycle");
 
-BillingCycle.methods(['put', 'post', 'put', 'delete'])
-BillingCycle.updateOptions({new: true, runValidators: true})
+BillingCycle.methods(["put", "post", "put", "delete"]);
+BillingCycle.updateOptions({ new: true, runValidators: true });
 
-module.exports = BillingCycle
+BillingCycle.route("get", (req, res, next) => {
+  BillingCycle.find({}, (err, docs) => {
+    if (!err) {
+      res.json(docs);
+    } else {
+      res.status(500).json({ errors: [error] });
+    }
+  });
+});
+
+BillingCycle.route("count", (req, res, next) => {
+  BillingCycle.count((error, value) => {
+    if(error) {
+      res.status(500).json({errors: [error]})
+    } else { 
+      res.json({value})
+    }
+  });
+});
+
+module.exports = BillingCycle;
