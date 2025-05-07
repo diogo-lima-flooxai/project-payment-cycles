36522b026b89dc6a1f50e5edadb07d1c6cedd52e
diogo <diogo.lima.flooxai@gmail.com>
2025-05-07 08:53:07 -0300

    feat: add functions to change, delete and some improvements with css

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -16,23 +16,44 @@ export function getList() {
 }
 
 export function create(values) {
-  return (dispatch) => {
-    axios
-      .post(`${BASE_URL}/billingCycles`, values)
-      .then((resp) => {
+  return submit(values, 'post')
+}
+
+export function update(values){
+  return submit(values, 'put')
+}
+
+export function remove(values){
+  return submit(values, 'delete')
+}
+
+function submit(values, method) {
+  return dispatch => {
+    const id = values._id ? values._id : ''
+    axios[method](`${BASE_URL}/billingCycles/${id}`, values)
+      .then(resp => {
         toastr.success("Sucesso", "Operação Realizada com sucesso");
         dispatch(init())
       })
       .catch((e) => {
-        e.response.data.errors.forEach((error) => toastr.error("Erro", error));
+        e.response.data.errors.forEach(error => toastr.error("Erro", error));
       });
   };
+  
 }
 
 export function showUpdate(billingCycle) {
   return [
     showTabs('tabUpdate'),
     selectTab('tabUpdate'),
+    initialize('billingCycleForm', billingCycle)
+  ]
+}
+
+export function showDelete(billingCycle) {
+  return [
+    showTabs('tabDelete'),
+    selectTab('tabDelete'),
     initialize('billingCycleForm', billingCycle)
   ]
 }
